3087945178176fc6eb6edbc9a376d992acf26373
Tim Heuett <tim.heuett@gmail.com>
2015-04-06 18:19:57 -0700

    fix test

--- test/puzzle_test.js
+++ test/puzzle_test.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect
 var Puzzle = require('../lib/puzzle.js');
 
-var emptyPuzzleRows = "+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+";
-var puzzleWithNumbers = "+---+---+---+\n|...|...|...|\n|...|2..|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|..3|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+";
+var emptyPuzzleRows = "+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n";
+var puzzleWithNumbers = "+---+---+---+\n|...|...|...|\n|...|2..|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|..3|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n";
 
 describe('puzzle', function() {
   var puzzle;
@@ -16,9 +16,9 @@ describe('puzzle', function() {
   });
 
   it('correctly displays numbers', function() {
-    puzzle.setNumber(1, 4, 2);
+    puzzle.setNumber(2, 4, 2);
     puzzle.setNumber(7, 6, 3);
 
-    expect(puzzle.printBoard()).to.equal(emptyPuzzleRows);
+    expect(puzzle.printBoard()).to.equal(puzzleWithNumbers);
   });
 });
