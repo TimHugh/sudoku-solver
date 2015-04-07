88e01da55daf8740ac70fbe29a6fdc54f55c3ef7
Tim Heuett <tim.heuett@gmail.com>
2015-04-07 10:05:47 -0700

    correctly test Puzzle.toArray function

--- test/puzzle_test.js
+++ test/puzzle_test.js
@@ -4,19 +4,32 @@ var Puzzle = require('../lib/puzzle.js');
 describe('Puzzle', function() {
   var puzzle;
 
+  var emptyPuzzle = [];
+  for(var i = 0; i < 81; i++) { emptyPuzzle[i] = 0; }
+
+  var puzzleWithNumbers = [];
+  for(var i = 0; i < 81; i++) { puzzleWithNumbers[i] = 0; }
+  puzzleWithNumbers[22] = 2;
+  puzzleWithNumbers[69] = 3;
+
   beforeEach(function() {
-    puzzle = new Puzzle;
+    puzzle = new Puzzle();
   });
 
   it('is created blank', function() {
-    for(space in puzzle.toArray) {
-      expect(space).to.equal(0);
+    for(var row = 0; row < puzzle.getHeight(); row++) {
+      for(var col = 0; col < puzzle.getWidth(); col++) {
+        expect(puzzle.getSpace(row, col)).to.equal(0);
+      }
     }
+    expect(JSON.stringify(puzzle.toArray())).to.equal(JSON.stringify(emptyPuzzle));
   });
 
-  it('correctly displays numbers', function() {
+  it('correctly saves and gets numbers', function() {
     puzzle.setSpace(2, 4, 2);
     puzzle.setSpace(7, 6, 3);
+
+    expect(JSON.stringify(puzzle.toArray())).to.equal(JSON.stringify(puzzleWithNumbers));
 
     expect(puzzle.getSpace(2, 4)).to.equal(2);
     expect(puzzle.getSpace(7, 6)).to.equal(3);
