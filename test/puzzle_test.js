f48c3b41f62814c654466c6fbdd54e997bdd275f
Tim Heuett <tim.heuett@gmail.com>
2015-04-06 18:10:26 -0700

    add set num test

--- test/puzzle_test.js
+++ test/puzzle_test.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect
 var Puzzle = require('../lib/puzzle.js');
 
 var emptyPuzzleRows = "+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+";
+var puzzleWithNumbers = "+---+---+---+\n|...|...|...|\n|...|2..|...|\n|...|...|...|\n+---+---+---+\n|...|...|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+\n|...|..3|...|\n|...|...|...|\n|...|...|...|\n+---+---+---+";
 
 describe('puzzle', function() {
   var puzzle;
@@ -11,6 +12,13 @@ describe('puzzle', function() {
   });
 
   it('is created blank', function() {
+    expect(puzzle.printBoard()).to.equal(emptyPuzzleRows);
+  });
+
+  it('correctly displays numbers', function() {
+    puzzle.setNumber(1, 4, 2);
+    puzzle.setNumber(7, 6, 3);
+
     expect(puzzle.printBoard()).to.equal(emptyPuzzleRows);
   });
 });
